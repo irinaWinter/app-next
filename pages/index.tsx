834d3cece205bf33fa516e573e38a6aa9afbc99f
Irina <loabanova.irina.96@mail.ru>
2021-11-24 14:17:41 +0300

    Добавляет компонент заголовка

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { Htag } from '../components';
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-      tyhgrtyhg
-    </div>
+    <>
+      <Htag tag="h1">Текст 1</Htag>
+      <Htag tag="h2">Текст 2</Htag>
+      <Htag tag="h3">Текст 3</Htag>
+    </>
   )
 }
