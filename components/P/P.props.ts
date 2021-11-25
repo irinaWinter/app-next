cb9193acc37413f5d20dd2f1ed03e70d6a45d49d
Irina <loabanova.irina.96@mail.ru>
2021-11-25 12:53:52 +0300

    Добавляет компонент абзаца

---
+++ components/P/P.props.ts
@@ -0,0 +1,6 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+export interface PProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
+    children: ReactNode;
+    size?: 's' | 'm' | 'l';
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -8,6 +8,9 @@ export default function Home(): JSX.Element {
       <Htag tag="h3">Текст 3</Htag>
       <Button appearance="primary" arrow="down">Кнопка</Button>
       <Button appearance="ghost" arrow="right">Кнопка</Button>
+      <P size="s">Текст small</P>
+      <P>Текст medium</P>
+      <P size="l">Текст large</P>
     </>
   )
 }
